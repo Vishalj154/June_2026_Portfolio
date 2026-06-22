d35fb9519595e7cd2524b53e374990416f6c3587
Vishalj154 <vishaljankar2534@gmail.com>
2026-06-22 11:23:52 +0530

    Added Google Analytics

---
+++ src/analytics.ts
@@ -0,0 +1,5 @@
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-8M3M38ZNQT");
+
+export default ReactGA;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-8M3M38ZNQT");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
